0c40a02776f75400f8b55fa8f7d40205de428def
Stavros Kefaleas <kefaleas.stavros@gmail.com>
2018-02-18 13:09:49 +0200

    Commit 1

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  //methods
+  public onSubmit(form: NgForm): void
+  {
+    console.log(form);
+  }
+
+}
